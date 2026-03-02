7a828996c38206a1922b1f059366107be30701f5
Xemasci <topkai903@gmail.com>
2026-03-02 22:47:39 +0700

    Create Reel.lua

---
+++ Reel.lua
@@ -0,0 +1,95 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/mamafoni281/KingRua-Library/refs/heads/main/Source"))()
+
+local Window = Library:NewWindow({
+    Title = "Cube Script [ v.0.1] ",
+    Description = "Made by: Everything"
+})
+
+local MainTab = Window:T("Main")
+local FarmSection = MainTab:AddSection("Auto Farm")
+
+-- Services
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ProximityPromptService = game:GetService("ProximityPromptService")
+local RemoteHandler = ReplicatedStorage:WaitForChild("RemoteHandler")
+
+local CollectRemote = RemoteHandler:WaitForChild("Collect")
+local FishingRemote = RemoteHandler:WaitForChild("Fishing")
+
+local AutoCollect = false
+
+FarmSection:AddToggle({
+    Title = "Auto Collect ",
+    Description = "Automatically collects Plot 5 to 10",
+    Default = false,
+    Callback = function(value)
+        AutoCollect = value
+        
+        task.spawn(function()
+            while AutoCollect do
+                for i = 1, 10 do
+                    CollectRemote:FireServer("Plot"..i)
+                    task.wait(0.2)
+                end
+                task.wait(1)
+            end
+        end)
+    end
+})
+
+FarmSection:AddButton({
+    Title = "Auto Upgrade",
+    Description = "Upgrades all plots instantly",
+    Callback = function()
+        for i = 1, 10 do
+            local upgradeString = "Brainrot3:Normal:" .. i .. ":1:2671523883"
+            BrainrotRemote:FireServer("Upgrade", upgradeString)
+            task.wait(0.3)
+        end
+    end
+})
+
+FarmSection:AddButton({
+    Title = "Instant Click",
+    Description = "Carry Brainrot",
+    Callback = function()
+        for _, prompt in pairs(workspace:GetDescendants()) do
+            if prompt:IsA("ProximityPrompt") then
+                fireproximityprompt(prompt)
+                task.wait(0.05) -- กันสแปม
+            end
+        end
+    end
+})
+
+local FishAmount = 2
+
+FarmSection:AddSlider({
+    Title = "Fishing Amount",
+    Description = "Amount sent to server",
+    Min = 1,
+    Max = 10,
+    Increment = 1,
+    Default = 2,
+    Callback = function(value)
+        FishAmount = value
+    end
+})
+
+local AutoFishing = false
+
+FarmSection:AddToggle({
+    Title = "Auto Fishing",
+    Description = "Automatically sends fishing event",
+    Default = false,
+    Callback = function(value)
+        AutoFishing = value
+        
+        task.spawn(function()
+            while AutoFishing do
+                FishingRemote:FireServer("Caught", FishAmount)
+                task.wait(0.5)
+            end
+        end)
+    end
+})
